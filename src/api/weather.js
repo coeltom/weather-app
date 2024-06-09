a696776fb1ee76b1651bce9a8a9408a81b311faf
Tomas <github@tomascoe.dev>
2024-06-09 02:52:15 -0300

    added env

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import 'dotenv/config';
 
 export const getWeatherByCoords = async (lat, lon) => {
   const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
+      import.meta.env.VITE_API_KEY
+    }&units=metric`
   );
   return response.data;
 };
