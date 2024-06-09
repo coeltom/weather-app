b7922899aa1376312eaafd9bee2038da5ea8886f
Tomas <github@tomascoe.dev>
2024-06-09 02:55:33 -0300

    exposed api key

--- src/api/weather.js
+++ src/api/weather.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 
 export const getWeatherByCoords = async (lat, lon) => {
   const response = await axios.get(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${
-      import.meta.env.VITE_API_KEY
-    }&units=metric`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=05a6793129d572e1ec8334b5653f1bd9&units=metric`
   );
   return response.data;
 };
